e4ede39b015b21a07314f8439933b490d30b2adc
Maksym Matsapura <mmatsapura2905@gmail.com>
2026-02-03 20:50:44 +0200

    add some tasks

--- code_examples/24_01/tasks.py
+++ code_examples/24_01/tasks.py
@@ -15,3 +15,36 @@
 # Create a new "secret" word by taking the first 3 letters and the last 3 letters of their word.
 # Print this new shortened version.
 # Example: If the word is "Keyboard", the output should be "Keyord".
+
+
+#----------------------------------------------------------
+# (04.02)
+# Task 1: The Substring Extractor Write a program that takes a string text = "PythonProgramming".
+# Extract and print the first 6 characters.
+# Extract and print the word "Programming" using a slice.
+# Print every second character of the string.
+# Reverse the string using slicing and print the result.
+
+
+
+# Task 2: List Trimming You have a list of numbers: numbers = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100].
+# Create a new list called middle_part that contains all elements from index 2 to index 7 (inclusive).
+# Create a list that contains only the last 3 elements of the original list.
+# Replace the elements from index 0 to 2 with the values [1, 2, 3].
+#
+
+
+# Task 3: Student Database Create a dictionary named student with the following keys: "name", "age", "city", and "subjects".
+# Set the values to: "Alice", 20, "New York", and a list ["Math", "Physics"].
+# Add a new key-value pair: "grade": "A".
+# Update the student's city to "London".
+# Remove the "age" key from the dictionary.
+# Print only the keys of the dictionary, and then print only the values.
+
+
+
+
+# Task 4: Inventory Manager Create a dictionary called inventory where keys are item names and values are their quantities: inventory = {"apples": 10, "bananas": 5, "oranges": 8}.
+# Write a script that checks if "grapes" is in the dictionary. If not, add it with a quantity of 12.
+# Increase the number of "apples" by 5.
+# Calculate and print the total number of items in the inventory (sum of all values).
